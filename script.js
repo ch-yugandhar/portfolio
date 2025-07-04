aefd637de91baa5b6d7592ebed5461f910a31dd6
Yugandhar Ch <chevitollayugandhargoud@gmail.com>
2025-07-04 19:21:53 +0530

    prototype-v1

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed("#typed", {
+    strings: [
+      "Web Developer.",
+      "Ethical Hacker.",
+      "I build. I secure. I innovate."
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true,
+    showCursor: true,
+    cursorChar: "|"
+  });
+});
